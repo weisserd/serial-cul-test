ed1900017435bc9c8750282f8206abc8cabc225c
Daniel Weisser <daniel.weisser@gmail.com>
2020-01-05 22:14:57 +0100

    Rewrite to different Java serial lib

--- src/main/java/de/danielweisser/home/SomfyCULTest.java
+++ src/main/java/de/danielweisser/home/SomfyCULTest.java
@@ -2,42 +2,46 @@ package de.danielweisser.home;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.openmuc.jrxtx.Parity;
-import org.openmuc.jrxtx.SerialPort;
-import org.openmuc.jrxtx.SerialPortBuilder;
+import com.fazecast.jSerialComm.SerialPort;
 
 /**
  * A simple test program to send commands to Somfy shutter via a CUL.
  * Documentation can be found at http://culfw.de/commandref.html#cmd_Y
- * 
+ *
  * @author Daniel Weisser
- * 
+ *
  */
 public class SomfyCULTest {
 
-	private static final Logger log = Logger.getLogger(SomfyCULTest.class.getName());
+    private static final Logger log = Logger.getLogger(SomfyCULTest.class.getName());
 
-	public static void main(String[] args) throws IOException {
-		log.info("Opening port ttyACM0");
-		SerialPort port = SerialPortBuilder.newBuilder("/dev/ttyACM0").setBaudRate(9600).setParity(Parity.NONE).build();
-		OutputStream out = port.getOutputStream();
+    public static void main(String[] args) throws IOException {
+        log.info("Opening port ttyACM0");
+        SerialPort comPort = SerialPort.getCommPort("/dev/ttyACM0");
+        comPort.openPort();
+        comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
 
-		String commandLEDOn = "l01\n";
-		String commandLEDOff = "l00\n";
-
-		String encryptionKey = "A1";
-		// C - Command (1 = My, 2 = Up, 4 = Down, 8 = Prog)
-		String command = "2";
-		String rollingCode = "001D";
-		String address = "000029";
-		String somfyCommand = "Ys" + encryptionKey + command + "0" + rollingCode + address + "\n";
-		out.write(somfyCommand.getBytes());
-		out.close();
-		port.close();
-
-		log.info("Closed port");
-	}
+        OutputStream out = comPort.getOutputStream();
+        try {
+            String encryptionKey = "A1";
+            // C - Command (1 = My, 2 = Up, 4 = Down, 8 = Prog)
+            String command = "4";
+            String rollingCode = "07F2";
+            String address = "000029";
+            String somfyCommand = "Ys" + encryptionKey + command + "0" + rollingCode + address + "\n";
+            log.info("Send code " + somfyCommand);
+            out.write(somfyCommand.getBytes());
+            long newRollingCode = Long.decode("0x" + rollingCode) + 1;
+            log.info("new rolling code: " + String.format("%04X", newRollingCode));
+            out.close();
+        } catch (Exception e) {
+            log.log(Level.SEVERE, e.getMessage(), e);
+        }
+        comPort.closePort();
+        log.info("Closed port");
+    }
 
 }
